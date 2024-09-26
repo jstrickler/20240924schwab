defa94cde7221bea1c4fc18aed0b62400cc085be
jstrickler <jstrickler@gmail.com>
2024-09-26 14:15:25 -0400

    added config_to_variables.py

---
+++ globals_examples.py
@@ -0,0 +1,17 @@
+
+person = "Abraham Lincoln"
+
+def spam():
+    word = "SPAM"
+    print(f"{word} {word} {word} {word} ")
+
+spam()
+print(person)
+
+g = globals()
+print(g)
+print('-' * 60)
+
+variable_name = "person"
+
+print(f"{g[variable_name] = }")
